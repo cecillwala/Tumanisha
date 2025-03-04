2e6a69bcce6d110d3afc24fe1259131649328d6e
cecillwala <lwalacecil13@gmail.com>
2025-03-04 07:22:40 +0000

    file-structure

---
+++ src/java/database/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBConnection {
+    static String db_URL = "jdbc:mysql://localhost:3306/users";
+    static String db_user = "root";
+    static String db_pasuwad = "admin";
+    
+    public static Connection getConnection() throws SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(db_URL, db_user, db_pasuwad);
+        }
+        catch(ClassNotFoundException e){
+            throw new SQLException("MySQL JDC driver not found!", e);
+        }
+    }
+}
