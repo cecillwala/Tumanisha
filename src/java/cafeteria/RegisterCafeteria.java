66227a801755acb5c09a960e991362c06f874a90
cecillwala <lwalacecil13@gmail.com>
2025-04-09 19:49:37 +0300

    INSERT query, angalia kwa RegisterCafeteria

--- src/java/cafeteria/RegisterCafeteria.java
+++ src/java/cafeteria/RegisterCafeteria.java
@@ -15,6 +15,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import database.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 /**
  *
@@ -27,6 +29,16 @@ public class RegisterCafeteria extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //TODO
+        
+    }   
+    
+    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //TODO
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String phone_no = request.getParameter("phoneNo");
@@ -36,31 +48,58 @@ public class RegisterCafeteria extends HttpServlet {
         
         try {
             Connection conn = DBConnection.getConnection();
-            String sql = "INSERT INTO users (firstName, lastName, phone_no, nationalID, cafeteria, password) VALUES (?, ?)";
+            String sql = "INSERT INTO users (first_name, last_name, username, phone_number, national_id, password, is_cafe_staff) VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, firstName);
             stmt.setString(2, lastName);
-            stmt.setString(3, phone_no);
-            stmt.setString(4, nationalID);
-            stmt.setString(5, cafeteria);
+            stmt.setString(3, lastName.toLowerCase() + "" + firstName.toLowerCase());
+            stmt.setString(4, phone_no);
+            stmt.setString(5, nationalID);
             stmt.setString(6, password);
+            stmt.setBoolean(7, true);
             stmt.executeUpdate();
             conn.close();
         }     catch (Exception e) {
             e.printStackTrace();
             response.getWriter().println("❌ Error: " + e.getMessage());
         }
-        String url = "cafeteria/home.jsp";
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
-        dispatcher.forward(request, response);
-    }   
-    
-    
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //TODO
+          int user_id = 0;
+          try {
+            Connection conn = DBConnection.getConnection();
+            //query to get data from db
+            String sql = "SELECT user_id FROM users WHERE national_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, nationalID);
+            //Hii ni kama array in a contain hiyo information
+            ResultSet rs = stmt.executeQuery();
+            
+            //Loops through the array and stores user id in variable
+            while (rs.next()) {
+                user_id = rs.getInt("user_id");
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("❌ Error: " + e.getMessage());
+        }
+          
+        try {
+            Connection conn = DBConnection.getConnection();
+            String sql = "INSERT INTO cafeterias (cafeteria_name, user_id) VALUES (?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            //Kuwa conscious of the data type "setString" "setInt"
+            stmt.setString(1, cafeteria);
+            stmt.setInt(2, user_id);
+            stmt.executeUpdate();
+            conn.close();
+        }     catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("❌ Error: " + e.getMessage());
+        }
+
     }
 
     /**
