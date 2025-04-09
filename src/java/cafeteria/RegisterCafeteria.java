d1f4e5470cd8c183c8fe9c83d9df7dab09ef7dd1
cecillwala <lwalacecil13@gmail.com>
2025-04-09 15:53:13 +0300

    Sign ups and Logins validation

--- src/java/cafeteria/RegisterCafeteria.java
+++ src/java/cafeteria/RegisterCafeteria.java
@@ -18,28 +18,10 @@ import utils.User;
  *
  * @author Admin
  */
-@WebServlet(name = "RegisterCafeteria", urlPatterns = {"/RegisterCafeteria"})
+@WebServlet(name = "RegisterCafeteria", urlPatterns = {"/cafeteria/RegisterCafeteria"})
 public class RegisterCafeteria extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -62,18 +44,10 @@ public class RegisterCafeteria extends HttpServlet {
     
     
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
+        //TODO
     }
 
     /**
